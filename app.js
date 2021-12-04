37e5160e2d2430d98089d7b9fa3fa0abb2db6b97
drewjya <andre.wijaya004@binus.ac.id>
2021-12-04 15:39:33 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import spoonacularRoutes from './Routes/SpoonacularRoute.js'; 
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/spoonacular', spoonacularRoutes);
 
 app.get('/', (reg, res, next) =>{
